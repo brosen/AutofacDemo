06809d21698539837dd2cdb65388de7f92221ffc
brosen <nywebman2@yahoo.com>
2018-11-23 13:29:29 -0500

    clean up extra usings

--- AutofacDemo/Controllers/AutoFacTestController.cs
+++ AutofacDemo/Controllers/AutoFacTestController.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using AutofacDemo.Model;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AutofacDemo.Controllers
--- AutofacDemo/Startup.cs
+++ AutofacDemo/Startup.cs
@@ -1,17 +1,12 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Autofac;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Autofac.Extensions.DependencyInjection;
-using Autofac;
 using AutofacDemo.Model;
 
 namespace AutofacDemo
